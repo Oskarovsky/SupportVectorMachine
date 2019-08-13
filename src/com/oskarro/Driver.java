c89af0a05b7ee967532ee94ea183276b10be93f2
oskarro <oskar.slyk@gmail.com>
2019-08-13 19:32:29 +0200

    Initial commit

---
+++ src/com/oskarro/Driver.java
@@ -0,0 +1,28 @@
+package com.oskarro;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+/**
+ * The SupportVectorMachine program implements an application that
+ * simply handles machine learning based on SVM
+ *
+ * @author  Oskar Slyk
+ * @version 1.0
+ * @since   2019-08-13
+ */
+
+
+public class Driver extends Application {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Platform.setImplicitExit(false);
+
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module javafx {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires commons.math3;
+
+    opens com.oskarro;
+}
